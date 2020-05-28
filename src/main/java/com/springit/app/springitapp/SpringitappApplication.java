11cd8a4f3d3fef2c7b8443ad778c0708e6c5c68f
MidasTouch <ronyjohn007@gmail.com>
2020-05-28 10:55:41 -0400

    Initial Commit

---
+++ src/main/java/com/springit/app/springitapp/SpringitappApplication.java
@@ -0,0 +1,13 @@
+package com.springit.app.springitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringitappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringitappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springit/app/springitapp/SpringitappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.springit.app.springitapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitappApplicationTests {
+
+/*    @Test
+    void contextLoads() {
+    }*/
+
+}
